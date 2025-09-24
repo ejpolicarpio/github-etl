a79cc09c7fd894226be8556d87b5a2c605063c0e
Erald <eraldjohnpolicarpio@gmail.com>
2025-09-24 22:47:13 +0400

    added schema for repository response and class for searching repositories.

---
+++ src/repository.py
@@ -0,0 +1,31 @@
+from src.configuration import Settings
+from typing import List
+import httpx
+
+from src.schema import RepositoryResponseSchema
+
+
+class GitHubClient:
+    def __init__(self, settings: Settings):
+        self.settings = settings
+        self.base_url = "https://api.github.com"
+        self.headers = {
+            "Accept": "application/vnd.github.v3+json",
+            "User-Agent": "github-etl/1.0"
+        }
+
+    async def search_repositories(self, query: str) -> List[RepositoryResponseSchema]:
+        async with httpx.AsyncClient() as client:
+            url =f"{self.base_url}/search/repositories"
+            params = {"q": query}
+
+            response = await client.get(url, headers=self.headers, params=params)
+            response.raise_for_status()
+
+            data = response.json()
+            repositories = []
+            for item in data.get("items", []):
+                repo = RepositoryResponseSchema(**item)
+                repositories.append(repo)
+            return repositories
+
---
+++ src/schema.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from pydantic import BaseModel
+
+class RepositoryResponseSchema(BaseModel):
+    id: int | None = None
+    name: str | None = None
+    full_name: str | None = None
+    description: str | None = None
+    stargazers_count: int | None = None
+    watchers_count: int | None = None
+    forks_count: int | None = None
+    created_at: datetime | None = None
+    updated_at: datetime | None = None
+    html_url: str | None = None
+    language: str | None = None
